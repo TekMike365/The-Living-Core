a95d313af6ef579d93294bb73647605bbce2b993
TekMike365 <miskosklenka@gmail.com>
2025-04-27 17:41:46 +0200

    moved 'TheCore' -> 'TheLivingCore'

--- src/main/java/tekmike365/tlc/TheLivingCore.java
+++ src/main/java/tekmike365/tlc/TheLivingCore.java
@@ -1,7 +1,13 @@
 package tekmike365.tlc;
 
 import net.fabricmc.api.ModInitializer;
-import tekmike365.tlc.core.TheCore;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.BlockPosArgumentType;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import com.mojang.brigadier.context.CommandContext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +18,33 @@ public class TheLivingCore implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		TheCore.initialize();
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(CommandManager.literal("vivere")
+					.requires(source -> source.hasPermissionLevel(1))
+					.then(CommandManager.argument("block", BlockPosArgumentType.blockPos())
+							.executes(TheLivingCore::executeVivereCommand)));
+			dispatcher.register(CommandManager.literal("morior")
+					.requires(source -> source.hasPermissionLevel(1))
+					.then(CommandManager.argument("block", BlockPosArgumentType.blockPos())
+							.executes(TheLivingCore::executeMoriorCommand)));
+		});
 	}
 
+	private static int executeVivereCommand(CommandContext<ServerCommandSource> context) {
+		ServerCommandSource source = context.getSource();
+
+		BlockPos blockPos = BlockPosArgumentType.getBlockPos(context, "block");
+		source.sendFeedback(() -> Text.literal("block: %s".formatted(blockPos.toString())), true);
+
+		return 0;
+	}
+
+	private static int executeMoriorCommand(CommandContext<ServerCommandSource> context) {
+		ServerCommandSource source = context.getSource();
+
+		BlockPos blockPos = BlockPosArgumentType.getBlockPos(context, "block");
+		source.sendFeedback(() -> Text.literal("block: %s".formatted(blockPos.toString())), true);
+
+		return 0;
+	}
 }
