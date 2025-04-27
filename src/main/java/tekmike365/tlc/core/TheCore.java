226f1152239dc8b383d940dcd2bb6930f2d2bcc9
TekMike365 <miskosklenka@gmail.com>
2025-04-27 17:32:36 +0200

    Added commands: 'vivere', 'morior'

--- src/main/java/tekmike365/tlc/core/TheCore.java
+++ src/main/java/tekmike365/tlc/core/TheCore.java
@@ -1,37 +1,44 @@
 package tekmike365.tlc.core;
 
-import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
 
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.BlockPosArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
 
 public class TheCore {
 
-    private static final Command<ServerCommandSource> COMMAND = context -> {
-        ServerCommandSource source = context.getSource();
-
-        ServerPlayerEntity player = source.getPlayer();
-        player.sendMessage(Text.literal("YEEEEEEEEET"));
-
-        return 0;
-    };
-
-    private static final Command<ServerCommandSource> COMMAND_2 = context -> {
-        ServerCommandSource source = context.getSource();
-
-        ServerPlayerEntity player = source.getPlayer();
-        player.sendMessage(Text.literal("YEEEEEEEEET x2"));
-
-        return 0;
-    };
-
     public static void initialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            dispatcher.register(CommandManager.literal("test_command").requires(source -> source.hasPermissionLevel(1)).executes(COMMAND)
-                    .then(CommandManager.literal("cmd2").executes(COMMAND_2)));
+            dispatcher.register(CommandManager.literal("vivere")
+                    .requires(source -> source.hasPermissionLevel(1))
+                    .then(CommandManager.argument("block", BlockPosArgumentType.blockPos())
+                            .executes(TheCore::executeVivereCommand)));
+            dispatcher.register(CommandManager.literal("morior")
+                    .requires(source -> source.hasPermissionLevel(1))
+                    .then(CommandManager.argument("block", BlockPosArgumentType.blockPos())
+                            .executes(TheCore::executeMoriorCommand)));
         });
+    }
+
+    private static int executeVivereCommand(CommandContext<ServerCommandSource> context) {
+        ServerCommandSource source = context.getSource();
+
+        BlockPos blockPos = BlockPosArgumentType.getBlockPos(context, "block");
+        source.sendFeedback(() -> Text.literal("block: %s".formatted(blockPos.toString())), true);
+
+        return 0;
+    }
+
+    private static int executeMoriorCommand(CommandContext<ServerCommandSource> context) {
+        ServerCommandSource source = context.getSource();
+
+        BlockPos blockPos = BlockPosArgumentType.getBlockPos(context, "block");
+        source.sendFeedback(() -> Text.literal("block: %s".formatted(blockPos.toString())), true);
+
+        return 0;
     }
 }
